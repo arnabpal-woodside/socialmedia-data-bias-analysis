cf4661ed9fc8f7210424e4753c10f182e6dfa2a9
bajajkarabi <38754429+bajajkarabi@users.noreply.github.com>
2020-08-31 12:54:55 +0530

    Update NaturalLangAnalysis.js

--- NaturalLangAnalysis.js
+++ NaturalLangAnalysis.js
@@ -1,77 +1,88 @@
-const config = require('./configNew'), 
- fs = require('fs');
+const fs = require('fs'),
+    tweetdata = require('./data.json'),
+     NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1'),
+    { IamAuthenticator } = require('ibm-watson/auth'),
+   naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+       version: '2019-07-12',
+       authenticator: new IamAuthenticator({
+           apikey: "LUg7iyRGq3m9qj_Nh95grcb3AgeZaWRIp9RBuWe8FVsY",
+       }),
+       url: "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/37f4e121-9f84-4eb1-8b87-69186e08327e"
+   });
+   
+var analyzeParams = {
+    'features': {
+        'keywords': {
+            'sentiment': true,
+            'emotion': true,
+            'limit': 10
+        }
+    }
+};
 
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
+module.exports = class NaturalLangAnalysis {
 
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-    version: '2019-07-12',
-    authenticator: new IamAuthenticator({
-        apikey: config.nlu_api_key,
-    }),
-    url: config.nlu_url
-});
+    constructor(config) {
+        this.config = config
+    }
 
-const tweetdata = require('./data.json');
+    async processTweetData() {
 
-async function processData() {
+        let response = [];
+        try {
+            for (var i = 0; i < tweetdata.length; i++) {
+                analyzeParams.text = tweetdata[i].text;
+                let userId = tweetdata[i].userInfo_id;
+                let jsonResp = await this.analyseSentiment(analyzeParams,userId);
 
-    let response = [];
-
-    let analyzeParams = {
-        'features': {
-            'keywords': {
-                'sentiment': true,
-                'emotion': true,
-                'limit': 10
+                console.log("Analysis done for Row number: ", i)
+                response.push(jsonResp);
             }
+        } catch (err) {
+            console.error(err);
         }
-    };
-    for (i = 0; i < tweetdata.length; i++) {
+        return response;
 
-        analyzeParams.text = tweetdata[i].text;
-        let userId = tweetdata[i].userInfo_id;
-        let jsonResp = await processNLU(analyzeParams, userId);
-        console.log("Analysis done for Row number: ", i)
-        response.push(jsonResp);
     }
-    console.log("Response ", JSON.stringify(response));
-    await writeFileJson(JSON.stringify(response))
-    console.log("Response Received");
-    return;
-}
+    async processRequest() {
+        let response = await this.processTweetData();
+        console.log("Response ", JSON.stringify(response));
+        await this.writeFileJson(JSON.stringify(response))
+        console.log("Response Received");
+        return response;
+    }
 
-async function writeFileJson(data) {
-    return new Promise(function (resolve, reject) {
-        fs.writeFile("output.json", data, function (err) {
-            if (err) reject(err);
-            else resolve(data);
+    async writeFileJson(data) {
+        return new Promise(function (resolve, reject) {
+            fs.writeFile("output.json", data, function (err) {
+                if (err) reject(err);
+                else resolve(data);
+            });
         });
-    });
-}
-
-async function processNLU(analyzeParams, userId) {
-    let finaljson = {};
-    try {
-        let analysisResults = await naturalLanguageUnderstanding.analyze(analyzeParams);
-
-        let jsonResponse = analysisResults.result.keywords[0];
-
-        if (jsonResponse != null) {
-            finaljson.userId = userId;
-            finaljson.text = analyzeParams.text;
-            finaljson.sentiment_label = jsonResponse.sentiment && jsonResponse.sentiment.label ? jsonResponse.sentiment.label : null;
-            finaljson.sadness = jsonResponse.emotion && jsonResponse.emotion.sadness ? jsonResponse.emotion.sadness : null;
-            finaljson.joy = jsonResponse.emotion && jsonResponse.emotion.joy ? jsonResponse.emotion.joy : null;
-            finaljson.fear = jsonResponse.emotion && jsonResponse.emotion.fear ? jsonResponse.emotion.fear : null;
-            finaljson.disgust = jsonResponse.emotion && jsonResponse.emotion.disgust ? jsonResponse.emotion.disgust : null;
-            finaljson.anger = jsonResponse.emotion && jsonResponse.emotion.anger ? jsonResponse.emotion.anger : null;
-        }
-    } catch (err) {
-        console.log('error:', err);
     }
-    return finaljson;
+
+    async analyseSentiment(analyzeParams, userId) {
+
+        let finaljson = {};
+        try {
+            let analysisResults = await naturalLanguageUnderstanding.analyze(analyzeParams);
+            let jsonResponse = analysisResults.result.keywords[0];
+            if (jsonResponse != null) {
+                finaljson.userId = userId;
+                finaljson.text = analyzeParams.text;
+                finaljson.sentiment_label = jsonResponse.sentiment && jsonResponse.sentiment.label ? jsonResponse.sentiment.label : null;
+                finaljson.sadness = jsonResponse.emotion && jsonResponse.emotion.sadness ? jsonResponse.emotion.sadness : null;
+                finaljson.joy = jsonResponse.emotion && jsonResponse.emotion.joy ? jsonResponse.emotion.joy : null;
+                finaljson.fear = jsonResponse.emotion && jsonResponse.emotion.fear ? jsonResponse.emotion.fear : null;
+                finaljson.disgust = jsonResponse.emotion && jsonResponse.emotion.disgust ? jsonResponse.emotion.disgust : null;
+                finaljson.anger = jsonResponse.emotion && jsonResponse.emotion.anger ? jsonResponse.emotion.anger : null;
+            }
+            return finaljson;
+        } catch (err) {
+            throw err;
+        }
+
+    }
+
 
 }
-
-processData();
